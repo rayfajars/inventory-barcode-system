10f2cd36afb525c7cf1a4174c09369fa675dbbe1
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-04-30 07:33:03 +0700

    feat: dahsboard filter fixed

--- resources/views/filament/widgets/stats-overview-widget-with-filters.blade.php
+++ resources/views/filament/widgets/stats-overview-widget-with-filters.blade.php
@@ -1,15 +1,15 @@
 <x-filament-widgets::widget>
     <div class="space-y-4 w-full">
-        <div class="p-4  rounded-xl w-full bg-white dark:bg-gray-900">
+        <div class="p-4  rounded-xl w-full bg-white dark:bg-gray-900 border-gray-500 border">
             <div class="flex flex-row w-full gap-4">
                 <div class="w-full">
-                    <label class="block text-sm font-medium  mb-2 text-black dark:text-white" >Business customers only</label>
+                    <label class="block text-sm font-medium  mb-2 text-black dark:text-white" >Pilih Product</label>
                     <x-filament::input.wrapper>
                         <x-filament::input.select
                             wire:model.live="selectedProduct"
                             class="w-full bg-gray-800 border-gray-700 text-black dark:text-white "
                         >
-                            <option value="">All Products</option>
+                            <option value="">Semua Products</option>
                             @foreach(\App\Models\Product::pluck('name', 'id') as $id => $name)
                                 <option value="{{ $id }}">{{ $name }}</option>
                             @endforeach
@@ -17,29 +17,33 @@
                     </x-filament::input.wrapper>
                 </div>
 
-                <div>
+                <div class="w-full">
                     <label class="block text-sm font-medium text-black dark:text-white mb-2">Tanggal Mulai</label>
-                    <x-filament::input
-                        type="date"
-                        wire:model.live="startDate"
-                        class="w-full bg-gray-800 border-gray-700 text-black dark:text-white"
-                    />
+                    <x-filament::input.wrapper>
+                        <x-filament::input
+                            type="date"
+                            wire:model.live="startDate"
+                        />
+                    </x-filament::input.wrapper>
                 </div>
 
-                <div>
+                <div class="w-full ">
                     <label class="block text-sm font-medium text-black dark:text-white mb-2">Tanggal Akhir</label>
-                    <x-filament::input
-                        type="date"
-                        wire:model.live="endDate"
-                        class="w-full bg-gray-800 border-gray-700 text-black dark:text-white"
-                    />
+                    <x-filament::input.wrapper>
+                        <x-filament::input
+                            type="date"
+                            wire:model.live="endDate"
+                        />
+                    </x-filament::input.wrapper>
                 </div>
             </div>
         </div>
 
-        <div class="grid grid-cols-3 gap-4">
+        <div class="flex flex-row gap-4 w-full">
             @foreach ($this->getStats() as $stat)
-                {{ $stat }}
+                <div class="flex-1">
+                    {{ $stat }}
+                </div>
             @endforeach
         </div>
     </div>
