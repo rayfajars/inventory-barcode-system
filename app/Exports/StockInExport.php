795c0cf537f494d435fc28a618d35729a32f7fe6
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-05-11 13:34:21 +0700

    feat: add export by filter log stok

--- app/Exports/StockInExport.php
+++ app/Exports/StockInExport.php
@@ -6,40 +6,48 @@ use App\Models\StockLog;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Illuminate\Database\Eloquent\Builder;
 
 class StockInExport implements FromCollection, WithHeadings, WithMapping
 {
-    protected $records;
+    protected $query;
 
-    public function __construct($records)
+    public function __construct(?Builder $query = null)
     {
-        $this->records = $records;
+        $this->query = $query;
     }
 
     public function collection()
     {
-        return $this->records;
+        if ($this->query) {
+            return $this->query->get();
+        }
+        return StockLog::where('type', 'in')->get();
     }
 
     public function headings(): array
     {
         return [
-            'Barcode',
             'Nama Produk',
+            'Barcode',
+            'Stock',
             'Harga',
-            'Stok',
-            'Batas Stok',
+            'Tipe',
+            'Processed By',
+            'Tanggal',
         ];
     }
 
     public function map($row): array
     {
         return [
-            $row->product->barcode,
             $row->product->name,
-            $row->product->price,
+            $row->product->barcode,
             $row->quantity,
-            $row->product->stock_limit,
+            'Rp ' . number_format($row->product->price, 0, ',', '.'),
+            $row->type === 'in' ? 'Masuk' : 'Keluar',
+            $row->user->name,
+            $row->created_at->format('d/m/Y H:i:s'),
         ];
     }
 }
